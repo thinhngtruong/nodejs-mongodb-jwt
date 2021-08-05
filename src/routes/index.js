a6635089b2290a94d1b1e11a3340eeff485fabe2
Thomas Nguyen <thomas@boostcommerce.net>
2021-08-06 02:12:12 +0700

    feat: update swagger for api document

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,14 @@ const AuthMiddleWare = require("../middleware/AuthMiddleware");
 const authRouter = require("./Auth.route");
 const tasksRouter = require("./Tasks.route");
 const usersRouter = require("./Users.route");
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+
+// Config swagger
+const swaggerFile = (process.cwd() + "/swagger/swagger.json");
+const swaggerData = fs.readFileSync(swaggerFile, 'utf8');
+const customCss = fs.readFileSync((process.cwd() + "/swagger/swagger.css"), 'utf8');
+const swaggerDocument = JSON.parse(swaggerData);
 /**
  * Init all APIs on your application
  * @param {*} app from express
@@ -11,11 +19,16 @@ const usersRouter = require("./Users.route");
 let initAPIs = (app) => {
 	// Lists Public API
 	router.use("/authentication", authRouter);
+
 	router.use("/users", usersRouter);
+
+	router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, null, null, customCss));
+
 	router.use(AuthMiddleWare.isAuth);
 
 	// List Protected APIs:
 	router.use("/tasks", tasksRouter);
+
 	return app.use("/api", router);
 }
 
